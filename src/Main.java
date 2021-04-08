5a6ed30238aff44cb8ed160451bf1e0bb0b1dbe0
Javier <jmnoeda2000@gmail.com>
2021-04-08 10:30:44 +0200

    Ejercicio no Terminado

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+
+import java.io.* ; // Gestiona las operaciones de entrada y de salida
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) throws IOException{
+        Scanner introduce = new Scanner(System.in);
+
+        // Declaracion de variables que usaremos mas adelante para introducir los datos nuevos
+        int nuevoId = 0;
+        String nuevoNombre = "";
+        String nuevaLocalidad = "";
+
+        File fichero = new File("Departamentos.dat");
+        DataOutputStream escritura = new DataOutputStream (new FileOutputStream(fichero,false));
+
+        //Datos originales del fichero
+        introducirDatos(1,"Laia","Barcelona",escritura);
+        introducirDatos(2,"Alex","Vallecas",escritura);
+        introducirDatos(3,"Miguel","Arganda",escritura);
+        introducirDatos(4,"Amane","Valdemoro",escritura);
+        introducirDatos(5,"Gon","Galapagar",escritura);
+        escritura.close();
+
+        DataInputStream lectura = new DataInputStream (new FileInputStream(fichero));
+
+        System.out.println("Que departamento quieres modificar");
+        nuevoId=introduce.nextInt();
+
+        if(exists(nuevoId,lectura)==true) {
+
+            System.out.println("Introduce el nombre nuevo: ");
+            nuevoNombre=introduce.next(); // Cuando son Strings solo se usa el next
+            System.out.println("Introduce la localidad: ");
+            nuevaLocalidad=introduce.next();
+
+            RandomAccessFile fich = new RandomAccessFile(fichero, "rw");
+
+            switch(nuevoId) {
+                case 1:
+                    fich.seek(0);
+                    System.out.println(fich.readInt());
+                    System.out.println(fich.readUTF());
+                    System.out.println(fich.readUTF());
+            }
+
+
+        } else {
+            System.out.println("El numero que introduces no existe como departamento");
+        }
+
+
+    }
+
+    // Metodo para introducir los datos dentro del fichero
+    public static void introducirDatos(int departamento, String nombre, String localidad, DataOutputStream escritura) throws IOException {
+        escritura.writeInt(departamento);
+        escritura.writeUTF(nombre.toString());
+        escritura.writeUTF(localidad.toString());
+    }
+
+    public static boolean exists(int nuevoId,DataInputStream lectura) throws IOException {
+        boolean buleano=false;
+        int prueba=0;
+        try{
+            while (lectura.available()>0) {
+                if(lectura.readInt()==nuevoId) {
+                    prueba++;
+                    System.out.println("--------------------------\nNUMERO DE DEPARTAMENTO: "+nuevoId+"\nNOMBRE: "+lectura.readUTF()+"\nLOCALIDAD: "+lectura.readUTF());
+
+
+                }else {
+                    lectura.readUTF();
+                    lectura.readUTF();
+                }
+            }
+            lectura.close();
+        } catch (EOFException e) {
+            System.out.println("No funciona");
+        }
+        if(prueba==1) {
+            buleano=true;
+        }
+        return buleano;
+    }
+}
